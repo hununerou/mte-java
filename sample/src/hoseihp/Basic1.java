688864f8f8cc933f892df5c6b2b153275c121e98
mte <hnnrplayer@gmail.com>
2017-03-04 23:11:28 +0900

    add hello Basic1

--- sample/src/hoseihp/Basic1.java
+++ sample/src/hoseihp/Basic1.java
@@ -8,6 +8,7 @@ public class Basic1{
 	
 	void start(){
 		JOptionPane.showMessageDialog(null, "1+2*3="+(1+2*3));	//1と2*3=6をただつなげている（10の位の1は1*10ではない）
+		System.out.println("hellooooooooo");
 	}
 }
 	
