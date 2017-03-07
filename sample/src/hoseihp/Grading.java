ab0334421a10fb9c692a8325d5fd758cbe524da0
mte <hnnrplayer@gmail.com>
2017-03-07 18:35:04 +0900

    Grading は 物理の最高得点は何点かを表示するプログラム追加

--- sample/src/hoseihp/Grading.java
+++ sample/src/hoseihp/Grading.java
@@ -1,5 +1,4 @@
 package hoseihp;
-
 import javax.swing.JOptionPane;
 
 public class Grading {
@@ -8,30 +7,43 @@ public class Grading {
 	}
 
 	void start() {
-		ScoreSheet[] aiueo = new ScoreSheet[3];
+		ScoreSheet[] aiueo = new ScoreSheet[3];	//三人分の名前、英語、数学、物理のスコアを用意する
 		for (int i = 0; i < aiueo.length; i++) {
-			aiueo[i] = getScoreSheets();
+			aiueo[i] = getScoreSheets();			//コンストラクタ内で名前とかスコア入れる呼び出しをする
 		}
 		JOptionPane.showMessageDialog(null,
 				"English average score is:" + (aiueo[0].English + aiueo[1].English + aiueo[2].English) / 3);
 
-		int count = 0;
+		int count = 0;								//数学60点未満晒す
 		for (int i = 0; i < aiueo.length; i++)
 			if (countMathDropouts(aiueo[i])) {
 				count++;
 			}
-		JOptionPane.showMessageDialog(null, "How many students drop Math's score are" + count);
-
-	}
+		JOptionPane.showMessageDialog(null, "How many students drop Math's border are:" + count);
+												// System.out.println(aiueo[0].Psysics);
+			//改善余地おおあり	物理の点数が最も高いものを表示する																									//for(int i = 0; i< aiueo.length; i++){
+																							//if(aiueo[0].Psysics > aiueo[1].Psysics){
+																							//	aiueo[1].Psysics = aiueo[0].Psysics																		
+																							//	}}
+				if(aiueo[0].Psysics > aiueo[1].Psysics){
+					if(aiueo[0].Psysics > aiueo[2].Psysics){
+						aiueo[2].Psysics = aiueo[0].Psysics;
+					}
+				}
+				else if(aiueo[1].Psysics > aiueo[2].Psysics){
+					aiueo[2].Psysics = aiueo[1].Psysics;
+				}
+		JOptionPane.showMessageDialog(null, "Psysics master is:"+aiueo[2].Psysics);
+		}
 
 	ScoreSheet getScoreSheets() {
-		String input = JOptionPane.showInputDialog("what's your name");
+		//String input = JOptionPane.showInputDialog("what's your name");
 		String Escore = JOptionPane.showInputDialog("what's your English score");
 		String Mscore = JOptionPane.showInputDialog("what's your Math score");
 		String Sscore = JOptionPane.showInputDialog("what's your Psysics score");
 
 		ScoreSheet r = new ScoreSheet();
-		r.name = input;
+		//r.name = input;
 		r.English = Integer.parseInt(Escore);
 		r.Math = Integer.parseInt(Mscore);
 		r.Psysics = Integer.parseInt(Sscore);
@@ -39,10 +51,13 @@ public class Grading {
 	}
 
 	boolean countMathDropouts(ScoreSheet a) {
-		System.out.println(a.Math);
+												// System.out.println(a.Math); 確認用
 		if (a.Math < 60) {
 			return true;
 		}
 		return false;
 	}
+	//ScoreSheet MaxPsysics(ScoreSheet hello){
+		
+	//}
 }
--- sample/src/hoseihp/Random.java
+++ sample/src/hoseihp/Random.java
@@ -1,53 +1,51 @@
 //あきらめやつ
 
 package hoseihp;
+
 import javax.swing.JOptionPane;
 
-public class Random{
-	public static void main(String args[]){
+public class Random {
+	public static void main(String args[]) {
 		new Random().start();
 	}
-	void start(){
-		double random = Math.random()*3;
+
+	void start() {
+		double random = Math.random() * 3;
 		System.out.println(random);
-		
+
 		String str = JOptionPane.showInputDialog("じゃけんじゃんけんしましょう");
 		String str1 = "";
 		String str2 = "";
-		
-		//CPUの出目
-		if(random<1.0)
+
+		// CPUの出目
+		if (random < 1.0)
 			str1 = "グー";
-		else if(1.0 < random && random < 2.0)
+		else if (1.0 < random && random < 2.0)
 			str1 = "チョキ";
-		else if(2.0 < random && random < 3.0)
+		else if (2.0 < random && random < 3.0)
 			str1 = "パー";
-		
-		//プレイヤーの出目
+
+		// プレイヤーの出目
 		int val = Integer.parseInt(str);
-		if(val == 1)
-		str2 = "グー"; 
-		else if(val == 2)
-		str2 = "チョキ";
-		else if(val == 3)
-			str2 = "パー"; 
-		
-		if(random < 1.0 && val == 3 ){		//グー
-			JOptionPane.showMessageDialog(null, "私は"+str1+"あなたは"+str2+"。あなたの、勝ち！");	
-			
-		}
-		else if(1.0 < random && random < 2.0 && val == 1){ //チョキ
-			JOptionPane.showMessageDialog(null, "私は"+str1+"あなたは"+str2+"。あなたの、勝ち！");
-		}
-		else if(2.0 < random && val ==2){   //パー
-			JOptionPane.showMessageDialog(null, "私は"+str1+"あなたは"+str2+"。あなたの、勝ち");
-		}
-		else if(str1 == str2){  //あいこ
-			JOptionPane.showMessageDialog(null, "私もあなたも"+str1);
+		if (val == 1)
+			str2 = "グー";
+		else if (val == 2)
+			str2 = "チョキ";
+		else if (val == 3)
+			str2 = "パー";
+
+		if (random > 1.0 && val == 3) { // グー
+			JOptionPane.showMessageDialog(null, "私は" + str1 + "あなたは" + str2 + "。あなたの、勝ち！");
+
+		} else if (1.0 < random && random < 2.0 && val == 1) { // チョキ
+			JOptionPane.showMessageDialog(null, "私は" + str1 + "あなたは" + str2 + "。あなたの、勝ち！");
+		} else if (2.0 < random && val == 2) { // パー
+			JOptionPane.showMessageDialog(null, "私は" + str1 + "あなたは" + str2 + "。あなたの、勝ち");
+		} else if (str1 == str2) { // あいこ
+			JOptionPane.showMessageDialog(null, "私もあなたも" + str1);
 			start();
-		}
-		else{						//プレイヤーの負け
-			JOptionPane.showMessageDialog(null, "私は"+str1+"あなたは"+str2+"。あなたの、負け！w");
+		} else { // プレイヤーの負け
+			JOptionPane.showMessageDialog(null, "私は" + str1 + "あなたは" + str2 + "。あなたの、負け！w");
 		}
 	}
 }
