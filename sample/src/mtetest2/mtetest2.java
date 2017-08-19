152cd1167b2cd45935d4210960e54e72f5497c58
mte <hnnrplayer@gmail.com>
2017-08-20 01:04:43 +0900

    コンストラクタ呼出の引数を変更

--- sample/src/mtetest2/mtetest2.java
+++ sample/src/mtetest2/mtetest2.java
@@ -2,25 +2,27 @@ package mtetest2;
 
 public class mtetest2 {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 
 		// make a cart
 		Cart c = new Cart();
 		
 		// set items to the cart
-		Item i6 = new Item("iPhone 6",80000);
-		Item i7 = new Item("iPhone 7",90000);
-		Item i8 = new Item("iPhone 8",100000);
+		Item i6 = new Item("iPhone 6", 80000);
+		Item i7 = new Item("iPhone 7", 90000);
+		Item i8 = new Item("iPhone 8", 100000);
+		//Cart c = new Cart(i6);
+		c.addItem(i6, 2);
+		
+		c.addItem(i7, 3);
 
-        c.addItem(i6,2);
-        c.addItem(i7,3);
-		c.addItem(i8,1);
-        
+		c.addItem(i8, 1);
+
 		// show cart
-        // {
-        //   {itemName, number, totalPrice}, {itemName, number, totalPrice}, ..., 
-        //   allTotalPrice
-        // }
+		// {
+		// {itemName, number, totalPrice}, {itemName, number, totalPrice}, ...,
+		// allTotalPrice
+		// }
 		System.out.println(c);
 	}
 }
